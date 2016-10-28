6f41319ee75244054eafd9d9534298fde10aea67
Manny G <mgithub7@gmail.com>
2016-10-27 23:44:49 -0700

    Final Submission

--- PA5_Gelyana_Vara/Main.Designer.cs
+++ PA5_Gelyana_Vara/Main.Designer.cs
@@ -142,6 +142,7 @@
             // ackBtn
             // 
             this.ackBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ackBtn.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ackBtn.Location = new System.Drawing.Point(120, 131);
             this.ackBtn.Margin = new System.Windows.Forms.Padding(2);
             this.ackBtn.Name = "ackBtn";
@@ -212,6 +213,7 @@
             this.Controls.Add(this.groupBox2);
             this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Main";
             this.Text = " ";
             ((System.ComponentModel.ISupportInitialize)(this.imageReminder)).EndInit();
